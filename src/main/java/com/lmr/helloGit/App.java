64644bc9295e7d22a2bbc235d4fccbecf67628cb
MingruiSucceed <874088731@qq.com>
2019-07-26 11:31:08 +0800

    测试 commit

--- src/main/java/com/lmr/helloGit/App.java
+++ src/main/java/com/lmr/helloGit/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("第一次修改文件");
         System.out.println("第二次修改测试");
+        System.out.println("commit");
     }
 }
