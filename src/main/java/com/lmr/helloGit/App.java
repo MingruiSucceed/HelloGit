5e33aa77b556d1d4a063ccabe8682a4c33d3295e
lei.cao <caolei@careate.cn>
2019-07-30 16:18:46 +0800

    2nd

--- src/main/java/com/lmr/helloGit/App.java
+++ src/main/java/com/lmr/helloGit/App.java
@@ -12,7 +12,8 @@ public class App
         System.out.println("第一次修改文件");
         System.out.println("第二次修改测试");
         System.out.println("commit");
-        System.out.println("this line is commit by charley");
+        System.out.println("this line is commit by charley, charley changed this line");
         System.out.println("更新=merge");
+	System.out.pringln("commit by charley again");
     }
 }
