f7c1862f31f02e1fa3d281bc29ee5ba0827a9c49
MingruiSucceed <874088731@qq.com>
2019-12-06 11:05:13 +0800

    解决冲突

--- src/main/java/com/lmr/helloGit/App.java
+++ src/main/java/com/lmr/helloGit/App.java
@@ -19,8 +19,12 @@ public class App
         System.out.println("更新=merge");
         System.out.println("commit by charley again");
         
+        System.out.println("cb1f300d-7d63-41f4-a0bc-58a0906c5f97");
+		System.out.println("cb1f300d-7d63-41f4-a0bc-58a0906c5f97");
         
-        List<String> list = new ArrayList<>();
+        List<String> list = new ArrayList<String>();
         System.out.println(list);
+        
+        
     }
 }
